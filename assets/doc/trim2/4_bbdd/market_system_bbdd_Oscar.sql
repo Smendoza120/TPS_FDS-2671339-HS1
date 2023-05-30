195cca511ee81901989a64003110dfed6cd0d741
Oscar980817 <oscar980817@hotmail.com>
2023-05-29 22:29:51 -0500

    base de datos para agregar valores

--- assets/doc/trim2/4_bbdd/market_system_bbdd_Oscar.sql
+++ assets/doc/trim2/4_bbdd/market_system_bbdd_Oscar.sql
@@ -115,4 +115,64 @@ CREATE TABLE  sales (
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
+-- ESPACIO PARA CATA
+
+
+
+
+-- ESPACIO PARA HAROLD
+
+
+
+
+-- ESPACIO PARA OSCAR PAPASITO RIKO  para update y delete si o si debe ser con where y con el id mysql ya esta configurado para que sea asi, ya que es por seguridad con otro parametro botara el error 1175
+
+INSERT INTO users (names,mail,phone)
+VALUES ("Oscar Aguirre","Oscarpro123@esto.com",3223142717),
+("Oscar Agasde","Osdasdasd123@esto.com",3223782717),
+("Oscar Aasdasde","Osadasdo123@esto.com",3143142717);
+
+#SELECT * FROM users;
+
+INSERT INTO customer (idusers)
+VALUES (1),
+(2);
+SELECT * FROM customer;
+
+INSERT INTO bill (creation_date)
+VALUES ("2023-05-29"),
+  ("2089-05-29"),
+  ("2023-05-30"),
+  ("2023-06-01");
+  
+SELECT * FROM bill;
+
+INSERT INTO daily_report (report_date)
+#ingresar datos o crear
+ VALUES ("2023-05-29"),
+  ("2089-05-29"),
+  ("2023-05-30"),
+  ("2023-06-01"),
+  ("2023-06-07"),
+  ("2023-08-01"),
+  ("2023-09-01"),
+  ("2023-10-01");
+# ESTO ES PARA BUSCAR:  SELECT * FROM daily_report WHERE report_date = "2023-06-01";
+#ESTO ES PARA ACTUALIZAR UPDATE daily_report SET report_date = "2075-05-29" WHERE iddaily_report = 5;
+#ESTO ES PARA BORRAR DELETE from daily_report WHERE iddaily_report = 1;
+#select * from daily_report;
+
+
+INSERT INTO inventory (storage,date_purchase,unit_price,due_date,product,quantity_products)
+VALUES("almacen","2023-05-29",5.000,"2030-05-29","Arroz",20),
+("almacen","2023-05-29",20.000,"2029-05-29","Aceite",5),
+("almacen","2023-05-29",5.00,"2029-05-29","Huevos",10),
+("almacen","2023-05-29",2.500,"2030-05-29","Tostadas",15),
+("almacen","2023-05-29",7.00,"2030-05-29","bom bom bum",25);
+#select * from inventory;
+
+INSERT INTO sales (quantity_sold,names_customer,product_sold,idcustomer,idbill,iddaily_report,idinventory)
+VALUES (15,"Oscar Aguirre","Arroz",2,2,2,2),
+(1,"Oscar Aguirre","Aceite",3,3,3,3);
+select * from sales;
 #DROP DATABASE market_system_bbdd;
