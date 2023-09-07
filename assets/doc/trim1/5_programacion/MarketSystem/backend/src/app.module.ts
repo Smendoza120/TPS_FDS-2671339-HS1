8b829197edfb1ba898859a1fd407c56fb3fbbb52
Harold Sanchez <sanchezharold13@gmail.com>
2023-09-06 20:59:33 -0500

    Se realizan cambios al backend

--- assets/doc/trim1/5_programacion/MarketSystem/backend/src/app.module.ts
+++ assets/doc/trim1/5_programacion/MarketSystem/backend/src/app.module.ts
@@ -1,25 +1,63 @@
+import * as joi from 'joi';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule, HttpService } from '@nestjs/axios';
+import { lastValueFrom } from 'rxjs';
 
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/entities/user.entity';
-import { Owner } from './owner/entities/owner.entity';
-import { Permissions } from './permissions/entities/permission.entity';
-import { Employee } from './employee/entities/employee.entity';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { SalesModule } from './sales/sales.module';
+import { InventoriesModule } from './inventories/inventories.module';
+import { BillsModule } from './bills/bills.module';
+import { DatabaseModule } from './database/database.module';
+
+import { enviroments } from './enviroments';
+import { OwnerModule } from './owner/owner.module';
+import { PermissionsModule } from './permissions/permissions.module';
+import { EmployeeModule } from './employee/employee.module';
+import { CustomerModule } from './customer/customer.module';
+import { AuthModule } from './auth/auth.module';
+import config from './config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'market_system',
-      entities: [User, Owner, Permissions, Employee],
-      synchronize: true,
-      logging: true,
+    ConfigModule.forRoot({
+      envFilePath: enviroments[process.env.NODE_ENV] || '.env',
+      load: [config],
+      isGlobal: true,
+      validationSchema: joi.object({
+        API_KEY: joi.string().required(),
+        DATABASE_NAME: joi.string().required(),
+        DATABASE_PORT: joi.number().required(),
+      }),
     }),
-    TypeOrmModule.forFeature([User, Owner, Permissions, Employee]),
+    UsersModule,
+    SalesModule,
+    InventoriesModule,
+    BillsModule,
+    HttpModule,
+    DatabaseModule,
+    OwnerModule,
+    PermissionsModule,
+    EmployeeModule,
+    CustomerModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: 'TASKS',
+      useFactory: async (http: HttpService) => {
+        const request = http.get(
+          'https://jsonplaceholder.typicode.com/posts/1',
+        );
+        const tasks = await lastValueFrom(request);
+        return tasks.data;
+      },
+      inject: [HttpService],
+    },
   ],
 })
 export class AppModule {}
