177bfbfe82b600f5832f34aa030c86390f88808f
HaroldSanchez120 <harold.sanchez@newsol.com.co>
2023-11-12 13:36:17 -0500

    Mejora en readme

---
+++ frontend/webapp/controller/dailySales/ListSales.controller.ts
@@ -0,0 +1,12 @@
+import Controller from "sap/ui/core/mvc/Controller";
+
+/**
+ * @namespace com.marketsystem.marketsystem.controller
+ */
+export default class ListSales extends Controller {
+
+    /*eslint-disable @typescript-eslint/no-empty-function*/
+    public onInit(): void {
+
+    }
+}
