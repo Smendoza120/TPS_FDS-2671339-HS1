ce34a2f74994f221d14a09f55ce3d87c3b7652e3
HaroldSanchez120 <harold.sanchez@newsol.com.co>
2023-11-26 09:30:10 -0500

    fix
    fix

--- backend/src/auth/dto/email.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
-import {ApiProperty} from "@nestjs/swagger";
-
-export class EmailResetDto {
-  @ApiProperty()  
-  @IsNotEmpty()
-  @IsEmail()
-  email: string;
-}
