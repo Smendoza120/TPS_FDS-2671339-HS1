bee435dc9959d255a4e76dd40a478c4aedc4bd72
HaroldSanchez120 <harold.sanchez@newsol.com.co>
2023-11-26 09:30:34 -0500

    fix

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,26 +1,18 @@
 import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
+import { UsersController } from './controllers/users.controller';
 
+import { UsersService } from './services/users.service';
+import { Providers } from './providers/providers';
 import { AppService } from './app.service';
 
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
-import { MsModule } from './ms/ms.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      cache: true,
-      expandVariables: true,
-    }),
-    DatabaseModule,
-    AuthModule,
-    MsModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DatabaseModule, AuthModule],
+  controllers: [AppController, UsersController],
+  providers: [...Providers,AppService, UsersService],
 })
 export class AppModule {}
